29981e897d9e57fb767b5832d6761af1cc49fea8
mertkankaraer <154523181+mertkankaraer@users.noreply.github.com>
2026-01-06 14:59:01 +0300

    Implement completion status toggle in TaskItemScreen

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskItemScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskItemScreen.kt
@@ -12,24 +12,24 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun TaskItemScreen() {
-
-    // TODO 1: Create a mutable state to hold completion status (Boolean)
-    // Initial value should be false
-
+    var isCompleted by rememberSaveable { mutableStateOf(false) }
     Column(modifier = Modifier.padding(16.dp)) {
-
         Text(
             text = "Submit Final Project",
             style = MaterialTheme.typography.titleLarge
         )
+        Text(
+            text = if (isCompleted) "tamamlandi" else "tamamlanmamis",
+        style = MaterialTheme.typography.bodyLarge,
+             modifier = Modifier.padding(top = 8.dp)
+        )
 
-        // TODO 2: Show text "Completed" or "Not Completed"
-        // depending on completion state
-
-        Button(
+Button
+    (
             onClick = {
-                // TODO 3: Toggle completion state
-            }
+                isCompleted = !isCompleted
+            },
+            modifier = Modifier.padding(top = 16.dp)
         ) {
             Text("Change Status")
         }
@@ -38,6 +38,4 @@ fun TaskItemScreen() {
 
 @Preview(showBackground = true)
 @Composable
-fun TaskItemPreview() {
-    // TODO: Call TaskItemScreen
-}
+fun TaskItemPreview() {TaskItemScreen() }
